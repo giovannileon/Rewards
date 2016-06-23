8c798b297790becec43707d1db42d3d7c4a1e869
Giovanni Leon <giovanni.leon@globant.com>
2016-06-23 16:40:33 -0500

    last changes

--- grails-app/controllers/rewards/CustomerController.groovy
+++ grails-app/controllers/rewards/CustomerController.groovy
@@ -4,45 +4,44 @@ import grails.transaction.Transactional;
 
 class CustomerController {
 	static scaffold = true
-	
+
 	def calculationService
 
 	def lookup(){
 		//def customerInstance = Customer.list(sort:"lastName", order:"desc", max: 10, offset:10)
 		//def customerInstance = Customer.findAllByLastName("Foster")
-		
+
 		//def customerInstance = Customer.findAllByTotalPoints(5,[sort:"lastName",order:"desc" ])
-		
+
 		//def customerInstance = Customer.findAllByLastNameLike("B%")
 		//def customerInstance = Customer.findAllByTotalPointsGreaterThan(3)
 		//def customerInstance = Customer.findAllByTotalPointsGreaterThanEquals(3,[sort:"totalPoints",order:"desc"])
-		
-		
+
+
 		//def customerInstance = Customer.findAllByTotalPointsBetween(2,4,[sort:"totalPoints",order:"desc"])
-		
+
 		//def customerInstance = Customer.findAllByFirstNameAndTotalPoints("Bo", 3)
-		
+
 		//def customerInstance = Customer.findAllByFirstNameAndTotalPoints("Bo", 3)
-		
+
 		def customerInstance = Customer.findAllByFirstNameLikeAndTotalPoints("B%", 3)
-		
+
 		//def customerInstance = Customer.findByPhone(params.id)
 		[customerInstanceList:customerInstance]
 	}
 
 	def checkin(){
-
 	}
 	def index(){
 		//use default view
 		params.max = 10;
 		[customerInstanceList: Customer.list(params), customerInstanceCount: Customer.count()]
 	}
-	
+
 	def create(){
 		[customerInstance: new Customer()]
 	}
-	
+
 	@Transactional
 	def save(Customer customerInstance){
 		customerInstance.save()
@@ -50,14 +49,14 @@ class CustomerController {
 		//y necesito definir show
 		redirect (action:"show", id:customerInstance.id)
 	}
-	
+
 	def show(Long id){
 		def customerInstance = Customer.get(id)
 		//manipular datos
 		customerInstance = calculationService.getTotalPoints(customerInstance)
 		//muestra el cliente con el id correspondiente
 		[customerInstance: customerInstance]
-		
+
 	}
 	@Transactional
 	def edit(Long id){
@@ -67,10 +66,10 @@ class CustomerController {
 	}
 	@Transactional
 	def update(Long id){
-		//aqui tiene dos versiones de customer que se deben mezclar 
+		//aqui tiene dos versiones de customer que se deben mezclar
 		// una para el cliente de la bd y el otro que viene por la view
 		def customerInstance = Customer.get(id)
-		
+
 		//sobreescribo los datos de la bd con los que llegan de la forma
 		customerInstance.properties = params
 		customerInstance.save()
@@ -83,7 +82,7 @@ class CustomerController {
 		customerInstance.delete()
 		redirect (action:"index")
 	}
-	
+
 	def customerLookup(Customer lookupInstance){
 		def (customerInstance,welcomeMessage)= calculationService.processCheckin(lookupInstance)
 		render (view:"checkin", model:[customerInstance: customerInstance, welcomeMessage:welcomeMessage])
@@ -92,8 +91,8 @@ class CustomerController {
 		def customerInstance = Customer.findByPhone(params.id)
 		[customerInstance:customerInstance]
 	}
-	def updateProfile(){
-	
+	def updateProfile(Customer customerInstance){
+		customerInstance.save()
+		render (view:"profile", model:[customerInstance:customerInstance])
 	}
-		
 }
